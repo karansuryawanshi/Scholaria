5045f031707720f4ca2417ea7f588a7c38f68f01
karansuryawanshi <karansuryawanshi717@gmail.com>
2025-03-04 23:10:05 +0530

    Folder sETUP

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+  return (
+    <div>
+      <p>Unprotected Page</p>
+    </div>
+  );
+};
+
+export default SignInPage;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -4,8 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="text-xl font-semibold text-neutral-600"> Hello Buddy</p>;
-      <Button className="font-thin hover:scale-110">Hello Buddy</Button>
+      <p>Protected Page</p>
     </div>
   );
 }
